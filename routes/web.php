b4e4e5d120da9262b7336cc26532189f148c7fd7
bryanhanggara <brynhnggra1903@gmail.com>
2024-12-02 22:31:13 +0700

    fix : role

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 Route::get('/agenda-list', [HomeController::class,'listAgenda'])->name('agenda.list');
-Route::get('/news-list', [HomeController::class,'listNews'])->name('new.list');
-Route::get('/agenda-detail/{slug}', [HomeController::class, 'agendaDetail'])->name('agenda.detail');
-Route::get('/berita/{slug}', [HomeController::class, 'newsDetail'])->name('news.detail');
+Route::get('/berita-list', [HomeController::class,'listNews'])->name('news.list');
+Route::get('/agenda-list/{slug}', [HomeController::class, 'agendaDetail'])->name('agenda.detail');
+Route::get('/berita-list/{slug}', [HomeController::class, 'newsDetail'])->name('news.detail');
 
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth','isAdmin'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','isAdmin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
